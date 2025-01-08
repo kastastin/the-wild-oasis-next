863de88e285c3dd95bc598393300bf22330dd7f2
kastastin <kastastin.main@gmail.com>
2025-01-08 12:48:33 +0200

    added creating new guest on first sign in

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,11 +1,32 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+import { getGuest, createGuest } from "@/app/_lib/data-service";
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [Google],
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existinGuest = await getGuest(user.email);
+
+        if (!existinGuest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+
+      return session;
     },
   },
   pages: {

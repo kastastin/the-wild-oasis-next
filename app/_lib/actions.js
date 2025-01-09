9f2943e78161bd1575f464e13df897587c6b4f6b
kastastin <kastastin.main@gmail.com>
2025-01-09 12:05:28 +0200

    added deleting a reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 
 import { supabase } from "@/app/_lib/supabase";
+import { getBookings } from "@/app/_lib/data-service";
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 
 export async function signInAction() {
@@ -39,4 +40,30 @@ export async function updateGuest(formData) {
   }
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteBooking(bookingId) {
+  const session = await auth();
+
+  if (!session) {
+    throw new Error("You must be signed in to update your profile.");
+  }
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You are not allowed to delete this booking.");
+  }
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    throw new Error("Booking could not be deleted.");
+  }
+
+  revalidatePath("/account/reservations");
 }
